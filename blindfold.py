502258aaa24c00eee1528f6fbe27c2fdd9d20b63
Dan Webb <danwwebb@outlook.com>
2025-02-11 20:08:22 +1000

    updated vars

--- blindfold.py
+++ blindfold.py
@@ -4,9 +4,9 @@ def random_gobbie_distributor():
     names = ["Webb", "Janke", "Pj", "Brunner"]
     random.shuffle(names)
     
-    receiver = random.choice(names)
+    cunt_what_gets_gobbed = random.choice(names)
 
-    print(f"Gobbie giver: Hunter Gobbie receiver: {receiver}")
+    print(f"Gobbie giver: Hunter Gobbie receiver: {cunt_what_gets_gobbed}")
 
 if __name__ == "__main__":
     random_gobbie_distributor()
